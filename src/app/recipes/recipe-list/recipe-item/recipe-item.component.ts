c3957e4eb753cfa9ecf7b019a34443bc16574fe6
omarammura <omarammoralm10@gmail.com>
2024-01-23 00:35:09 +0300

    add Angular Routes.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import {RecipeService} from "../../recipe.service";
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,12 +7,11 @@ import {RecipeService} from "../../recipe.service";
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit{
-  @Input() recipe : Recipe
-  constructor(private recipeService:RecipeService){}
+  @Input() recipe : Recipe;
+  @Input() index:number;
+
+
   ngOnInit(): void {
 
-  }
-  onSelected(){
-    this.recipeService.recipeSelector.emit(this.recipe);
   }
 }
